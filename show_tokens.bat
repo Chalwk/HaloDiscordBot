54581970f1aa73ed6c06d67fce2418ab5549f2e9
Chalwk77 <chalwk.dev@gmail.com>
2026-05-14 01:44:51 +1200

    ...

--- show_tokens.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ============================================
-echo      Environment Variable Tokens
-echo ============================================
-echo.
-echo Discord Token: %DISCORD_TOKEN%
-echo GitHub Token:  %GITHUB_TOKEN%
-echo.
-pause
